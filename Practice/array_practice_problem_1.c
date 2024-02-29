cb19aa8ed7bffa84500159c6aa601268219e0448
Max29087 <topusorker087@gmai.com>
2024-02-29 12:56:13 +0600

    Delet comment

--- Practice/array_practice_problem_1.c
+++ Practice/array_practice_problem_1.c
@@ -7,18 +7,12 @@ int main()
     int array[n];
     for (int i = 0; i < n; i++)
     {
-        printf("Enter The valu of index %d :",i);
-        scanf("%d",&array[i]);
+        printf("Enter The valu of index %d :", i);
+        scanf("%d", &array[i]);
     }
-    for (int i = 0,k=n-1; i < n; i++,k--)
+    for (int i = 0, k = n - 1; i < n; i++, k--)
     {
-        //array[k]=array[i];
-        printf("%d\n",array[k]);
+        printf("%d\n", array[k]);
     }
-    //for (int i = 0; i <n; i++)
-    //{
-    //    printf("index%d: %d\n",i,array[i]);
-    //}
-    
     return 0;
 }
